a9e131168eef209b897489f93f700a55a13e279b
Faenya <44199418+Faenya@users.noreply.github.com>
2020-04-27 14:58:41 +0200

    ✨ add generated link preview (#7)
    Fix #6

--- web/src/links.jsx
+++ web/src/links.jsx
@@ -5,32 +5,40 @@ import {
   Edit,
   Create,
   SimpleForm,
+  FunctionField,
   TextField,
   EditButton,
   TextInput,
   required,
 } from "react-admin";
 
+if (!process.env.ABSOLUTE_LINK_PREFIX) {
+  throw new Error("ABSOLUTE_LINK_PREFIX is not set");
+}
+
+const absoluteLinkPrefix = process.env.ABSOLUTE_LINK_PREFIX
+
 export const PostList = (props) => (
   <List {...props}>
     <Datagrid>
-      <TextField source="incoming_link" />
-      <TextField source="outgoing_link" />
+      <FunctionField source="incoming_link" label="Generated link" render={record => toAbsoluteIncomingLink(record.incoming_link)}/>
+      <TextField source="outgoing_link" label="Destination" />
       <EditButton basePath="/links" />
     </Datagrid>
   </List>
 );
 
 const PostTitle = ({ record }) => {
-  return <span>Link {record ? `"${record.incoming_link}"` : ""}</span>;
+  return <span>Link {record ? `"${toAbsoluteIncomingLink(record.incoming_link)}"` : ""}</span>;
 };
 
 export const PostEdit = (props) => (
   <Edit title={<PostTitle record={props.record} />} {...props}>
     <SimpleForm>
-      <TextInput disabled source="id" />
-      <TextInput source="incoming_link" validate={[required()]} />
-      <TextInput source="outgoing_link" validate={[required()]} />
+      <TextInput disabled source="id" fullWidth/>
+      <TextInput source="incoming_link" label="Preview"  format={toAbsoluteIncomingLink} disabled fullWidth />
+      <TextInput source="incoming_link" label="Generated link" validate={[required()]} fullWidth/>
+      <TextInput source="outgoing_link" label="Destination" validate={[required()]} fullWidth/>
     </SimpleForm>
   </Edit>
 );
@@ -38,8 +46,14 @@ export const PostEdit = (props) => (
 export const PostCreate = (props) => (
   <Create title="Create a Post" {...props}>
     <SimpleForm>
-      <TextInput source="incoming_link" validate={[required()]} />
-      <TextInput source="outgoing_link" validate={[required()]} />
+      <TextInput source="incoming_link" label="Preview"  format={toAbsoluteIncomingLink} disabled fullWidth />
+      <TextInput source="incoming_link" label="Generated link" validate={[required()]} fullWidth/>
+      <TextInput source="outgoing_link" label="Destination" validate={[required()]} fullWidth/>
     </SimpleForm>
   </Create>
 );
+
+
+function toAbsoluteIncomingLink(relativeIncomingLink) {
+  return relativeIncomingLink ? absoluteLinkPrefix + relativeIncomingLink : ''
+}
