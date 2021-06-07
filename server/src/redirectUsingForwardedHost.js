22536f709a941fe1d5f6b8133280cfbe592e8c2f
Hugo Wood <hugo.wood@zenika.com>
2021-06-07 10:21:55 +0200

    🚑 fix not found when looking for ui
    The forwarded host feature was handling all requests 😬

--- server/src/redirectUsingForwardedHost.js
+++ server/src/redirectUsingForwardedHost.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 module.exports = ({ mapping }) =>
-  express.Router().get(/.*/, async (req, res) => {
+  express.Router().get(/.*/, async (req, res, next) => {
     const forwardedHost =
       req.headers["x-forwarded-host"] || req.headers["host"];
+    if (
+      forwardedHost === "links.zenika.com" ||
+      forwardedHost?.startsWith("localhost")
+    ) {
+      next();
+      return;
+    }
     if (!forwardedHost || !mapping[forwardedHost]) {
       res.sendStatus(404);
     } else {
