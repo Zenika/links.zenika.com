d93f88e7dd8e9684ffe193abac2382559fbfa040
Hugo Wood <hugo.wood@zenika.com>
2022-03-21 14:51:30 +0100

    ✨ support path forwarding for forwarded host redirects

--- server/src/redirectUsingForwardedHost.js
+++ server/src/redirectUsingForwardedHost.js
@@ -14,9 +14,17 @@ module.exports = ({ mapping }) =>
     if (!forwardedHost || !mapping[forwardedHost]) {
       res.sendStatus(404);
     } else {
+      const baseLocation =
+        typeof mapping[forwardedHost] === "string"
+          ? mapping[forwardedHost]
+          : mapping[forwardedHost].location;
+      const path = mapping[forwardedHost].forwardPath
+        ? new URL(req.url, baseLocation).pathname
+        : "";
+      const location = baseLocation + path;
       console.log(
-        `INFO redirect forwarded host '${forwardedHost}' to '${mapping[forwardedHost]}'`
+        `INFO redirect forwarded host '${forwardedHost}' to '${location}'`
       );
-      res.redirect(mapping[forwardedHost]);
+      res.redirect(location);
     }
   });
